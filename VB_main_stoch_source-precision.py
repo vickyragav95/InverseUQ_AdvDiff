c943b1f5eb73fc4f221022d06dcb380c87f1a92d
vickyragav95 <vickyragav95@gmail.com>
2020-12-03 10:28:06 -0500

    Added plots to be saved for each run

--- VB_main_stoch_source-precision.py
+++ VB_main_stoch_source-precision.py
@@ -51,44 +51,6 @@ def ln_likelihood_poly_fit(data_experiment, params):
 
     return -a[1]/(2.0*a[2]), 2.0*np.abs(a[2]), a[0]-a[1]*a[1]/(4.0*a[2])
 
-# def ln_Fpost_poly_fit(data_experiment, params, mu_f, sigsq_f):
-#     Nsamples = 10
-#     samples = np.random.uniform(0.0, 20.0, Nsamples)
-#     y = np.zeros((Nsamples,1), dtype=float)
-#     A = np.zeros((Nsamples,3), dtype=float)
-#     for i in range(Nsamples):
-#         if (len(params)==2):
-#             new_params = [samples[i], params[1]]
-#         else:
-#             new_params = [samples[i]]
-#         y[i,0] = ln_likelihood_simple(data_experiment, new_params)
-#         y[i,0] += -0.5*((samples[i]-mu_f)**2)/sigsq_f
-#         A[i,:] = [1, samples[i], samples[i]**2]
-#
-#     y = np.matmul(np.transpose(A), y)
-#     A = np.matmul(np.transpose(A), A)
-#     a = np.linalg.solve(A,y)
-#
-#     return -a[1]/(2.0*a[2]), 2.0*np.abs(a[2]), a[0]-a[1]*a[1]/(4.0*a[2])
-#
-# def ln_likelihood_MC(data_experiment, params, mu_f, sigsq_f):
-#     Nsamples = 100
-#     samples = np.random.normal(mu_f, np.sqrt(sigsq_f), Nsamples)
-#     y = np.zeros((Nsamples,1), dtype=float)
-#     for i in range(Nsamples):
-#         if (len(params)==2):
-#             new_params = [samples[i], params[1]]
-#         else:
-#             new_params = [samples[i]]
-#         y[i,0] = ln_likelihood_simple(data_experiment, new_params)
-#         y[i,0] += -0.5*((samples[i]-mu_f)**2)/sigsq_f
-#
-#     y = np.exp(y)
-#     mu_like = np.mean(y)
-#     tau_like = 1.0/(np.var(y))
-#
-#     return mu_like, tau_like
-
 np.random.seed(12345)
 
 n_data = 50
@@ -96,10 +58,10 @@ n_data = 50
 # true source
 source_true = 10.0
 # true noise added to data
-sig_true = 1.0
+sig_true = 2.0
 
 # deterministic parameters
-kappa_deterministic    = 0.1
+kappa_deterministic    = 0.2
 beta_deterministic     = 2.0
 bc_left_deterministic  = 0.5
 bc_right_deterministic = 0.0
@@ -118,8 +80,12 @@ tau_f = 2.0
 mu_f = 14.0
 a_0_tau = 2.0
 b_0_tau = 0.25
-
-for k in range(100):
+Niter=100
+mean_f = np.zeros([Niter+1,1])
+mean_tau = np.zeros([Niter+1,1])
+mean_f[0] = mu_f
+mean_tau[0] = a_0_tau/b_0_tau
+for k in range(Niter):
     nrml_lkhood_params = np.array([1.0, 1.0/Exp_tau_y])
     [mu_like, tau_like, off_like] = ln_likelihood_poly_fit(data_experiment, nrml_lkhood_params)
     # [mu_like, tau_like] = ln_likelihood_MC(data_experiment, nrml_lkhood_params, mu_f, 1.0/tau_f)
@@ -133,12 +99,13 @@ for k in range(100):
     tau_f = tau_N
     mu_f = mu_N
 
+    mean_f[k+1] = mu_N
+
     Exp_f = mu_N
     Exp_f_sq = mu_N**2 + 1.0/tau_N
 
     tau_like_tmp = tau_like/Exp_tau_y
     off_like_tmp = off_like/Exp_tau_y
-
     a_N_tau = a_0_tau + n_data/2 + 1
     b_N_tau = b_0_tau + 0.5*tau_like_tmp*Exp_f_sq - tau_like_tmp*mu_like*Exp_f + 0.5*tau_like_tmp*mu_like**2 - off_like_tmp
     # b_N = b_0_tau + 0.5*tau_like_tmp*Exp_f_sq - tau_like_tmp*mu_like*Exp_f + 0.5*tau_like_tmp*mu_like**2
@@ -147,12 +114,32 @@ for k in range(100):
     b_0_tau = b_N_tau
     Exp_tau_y = a_N_tau/b_N_tau
 
+    mean_tau[k+1] = a_N_tau/b_N_tau
+
     print('ITERATION=',k)
-    print(mu_N)
-    print(Exp_tau_y)
+    print('f=',[mu_N, tau_N])
+    print('tau=',[Exp_tau_y, Exp_tau_y/b_N_tau])
     print('\n')
 
-Npts = 500
+
+plot_each_chain = True
+if(plot_each_chain):
+    fig,axes = plt.subplots(2, 1, sharex=True)
+
+    axes[0].plot(mean_f, color='k', drawstyle='steps')
+    axes[1].plot(mean_tau, color='k', drawstyle='steps')
+
+    axes[0].axhline(source_true, color='r', label='source_true')
+    axes[0].legend(loc='best')
+    axes[0].set_ylabel('$\mathbb{E}[f]$')
+
+    axes[1].axhline(1/sig_true**2, color='r', label='sigma_true')
+    axes[1].legend(loc='best')
+    axes[1].set_ylabel('$\mathbb{E}[Tau_y]$')
+    plt.savefig('VB_f-sig_chains.png')
+    plt.show()
+
+Npts = 250
 x1 = np.linspace(6.0, 14.0, Npts)
 x2 = np.linspace(0.01, 2.0, Npts)
 X1, X2 = np.meshgrid(x1,x2)
@@ -170,16 +157,19 @@ plt.figure()
 plt.contour(X1, X2, Z)
 plt.axvline(source_true, color='r', linestyle='--', label='true')
 plt.axhline(1/sig_true**2, color='b', linestyle='--', label='true')
+plt.savefig('VB_f-sig_joint_dist.png')
 plt.show()
 
 plt.figure()
 p_source = norm.pdf(x1, mu_N, 1.0/tau_N)
 plt.plot(x1, p_source)
 plt.axvline(source_true, color='r', linestyle='--', label='true')
+plt.savefig('VB_f-sig_f_dist.png')
 plt.show()
 
 plt.figure()
 p_tau = gamma.pdf(x2, a_N_tau, 0.0, 1.0/b_N_tau)
 plt.plot(x2, p_tau)
 plt.axvline(1/sig_true**2, color='r', linestyle='--', label='true')
+plt.savefig('VB_f-sig_sig_dist.png')
 plt.show()
